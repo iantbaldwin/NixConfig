012a9b666c413e2c5e1afd25888b526f54dbe51c
itbaldwi <itbaldwi@ncsu.edu>
2016-02-16 05:31:34 -0500

    Fixed install script

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+curDir=$(pwd)
+echo $curDir
 
 # Install vimrc and bash_profile
-ln -s vimrc ~/.vimrc
-ln -s bash_profile ~/.bash_profile
+ln -sF $curDir/vimrc ~/.vimrc
+ln -sF $curDir/bash_profile ~/.bash_profile
 
 # Install VIM filetype plugins
-ln -s ftplugin/c.vim ~/.vim/ftplugin/c.vim
-ln -s ftplugin/java.vim ~/.vim/ftplugin/java.vim
+ln -sF $curDir/ftplugin/c.vim ~/.vim/ftplugin/c.vim
+ln -sF $curDir/ftplugin/java.vim ~/.vim/ftplugin/java.vim
 
 # Copy YCM_CONF
-ln -s ycm_extra_conf.py ~/.vim/.ycm_extra_conf.py
+ln -sF $curDir/ycm_extra_conf.py ~/.vim/.ycm_extra_conf.py
