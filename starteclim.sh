fa5b5b279f445d8103b4cb153713a664326fd59e
Ian Baldwin <iantbaldwin@elektrikfish.com>
2016-02-19 17:17:02 -0500

    Changed starteclim

--- starteclim.sh
+++ starteclim.sh
@@ -1,1 +1,1 @@
-/opt/homebrew-cask/Caskroom/eclipse-java/4.5.1/Eclipse.app/Contents/Eclipse/eclimd & > ~/.eclim.log
+/opt/homebrew-cask/Caskroom/eclipse-java/4.5.1/Eclipse.app/Contents/Eclipse/eclimd > ~/.eclim.log 2>&1 &
