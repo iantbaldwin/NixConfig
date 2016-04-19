be00af8039fca873c26a76f67521b17d60d64bf3
itbaldwi <itbaldwi@ncsu.edu>
2016-04-19 18:45:39 -0400

    Organized project folder

--- install.sh
+++ install.sh
@@ -3,42 +3,49 @@
 curDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 usrshell=$(basename $SHELL)
 
-homeColor="\"[32m\""
-remoteColor="\"[31m\""
-colorReset="\"[0m\""
+if [ "$usrshell" == "bash" ]; then
+	localColor="\"[32m\""
+	remoteColor="\"[31m\""
+elif [ "$usrshell" == "fish" ]; then
+	localColor="green"
+	remoteColor="red"
+fi
 
 # Link vimrc
-ln -sF $curDir/vimrc $HOME/.vimrc
+ln -sF $curDir/Config/Vim/vimrc $HOME/.vimrc
 
 # Create vim directories if needed
 mkdir -p $HOME/.vim
 mkdir -p $HOME/.vim/ftplugin
 
 # Link VIM filetype plugins
-ln -sF $curDir/ftplugin/c.vim $HOME/.vim/ftplugin/c.vim
-ln -sF $curDir/ftplugin/java.vim $HOME/.vim/ftplugin/java.vim
-ln -sF $curDir/ftplugin/asm.vim $HOME/.vim/ftplugin/asm.vim
+ln -sF $curDir/Config/Vim/ftplugin/c.vim $HOME/.vim/ftplugin/c.vim
+ln -sF $curDir/Config/Vim/ftplugin/java.vim $HOME/.vim/ftplugin/java.vim
+ln -sF $curDir/Config/Vimftplugin/asm.vim $HOME/.vim/ftplugin/asm.vim
 
 # Link YCM_CONF
-ln -sF $curDir/ycm_extra_conf.py $HOME/.vim/.ycm_extra_conf.py
+ln -sF $curDir/Config/Vim/ycm_extra_conf.py $HOME/.vim/.ycm_extra_conf.py
 
 # Link shell profile
+mkdir -p $curDir/Compiled
 if [ "$usrshell" == "fish" ]; then
 	mkdir -p $HOME/.config
-	printf "set INSTPATH $curDir \n" > config.fish
-	cat fishconfig >> config.fish
-	ln -sF $curDir/config.fish $HOME/.config/fish/config.fish
+	printf "#!/$(which fish)\n
+set remoteColor $remoteColor \n
+set localColor $localColor \n
+set INSTPATH $curDir \n" > $curDir/Compiled/config.fish
+	cat $curDir/Config/Fish/fish.config >> $curDir/Compiled/config.fish
+	ln -sF $curDir/Compiled/config.fish $HOME/.config/fish/config.fish
 elif [ "$usrshell" == "bash" ]; then
 	printf "#!/bin/bash\n
-homeColor=$homeColor\n
+homeColor=$localColor\n
 remoteColor=$remoteColor\n
-RESET=$colorReset\n
-NIXCONFIG=$curDir \n" > bash_profile
-	cat bashconfig >> bash_profile
+NIXCONFIG=$curDir \n" > $curDir/Compiled/bash_profile
+	cat $curDir/Config/Bash/bash.config >> $curDir/Compiled/bash_profile
 	ncsu=$(uname -a | grep -c ncsu)
 	if [ "$ncsu" == "1" ]; then
-		ln -sF $curDir/bash_profile $HOME/.mybashrc
+		ln -sF $curDir/Compiled/bash_profile $HOME/.mybashrc
 	else
-		ln -sF $curDir/bash_profile $HOME/.bash_profile
+		ln -sF $curDir/Compiled/bash_profile $HOME/.bash_profile
 	fi
 fi
