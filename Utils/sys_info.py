2460b3f0ddfa98df3065b16eb6c14f60817a96d5
Ian Baldwin <iantbaldwin@elektrikfish.com>
2016-04-19 23:59:48 -0400

    Update to sys_info

--- Utils/sys_info.py
+++ Utils/sys_info.py
@@ -1,4 +1,23 @@
 #!/usr/bin/python
+"""
+Prints system information based
+
+  - Gets the count of processes
+  - Gets the current date
+  - Gets the load averages
+  - Get storage information
+  - Get memory usage
+
+
+Author:     Ian Baldwin (iantbaldwin@elektrikfish.com)
+Version:    1.0
+Date:       4/19/2016
+
+Changes:
+--------
+Original version
+
+"""
 import subprocess
 import sys
 
@@ -6,16 +25,14 @@ def runCommand( command ):
     process = subprocess.Popen( command, shell=True, stdout=subprocess.PIPE )
     return process.stdout.read().replace( "\n", "" )
 
-
 #updates = runCommand( "brew outdated | wc | awk '{ print $1 }'" )
 processes = runCommand( "ps aux | wc | awk '{ print $1-6}'" )
 date = runCommand( "date" )
 load = runCommand( "sysctl -n vm.loadavg | awk '{ print $2, $3, $4 }'" ).replace( " ", ", " )
 strg = runCommand( "df | grep ' /$' | awk '{ printf \"%s of %.2fGB\", $5, $2*512/1000000000 }'" )
-freeMem = float( runCommand( "vm_stat | grep 'Pages free:' | awk '{ print $3*4096/1024^3 }'" ) )
+freeMem = float( runCommand( "vm_stat | grep 'Pages free:' | awk '{ print $3*4096/1024^3 }'" ) ) * 2
 ttlMem = float( runCommand( "sysctl -a | grep memmap.Conventional | awk '{ print $2/1024^3 }'" ) )
-
 
 print( "\n  System information as of {}\n".format( date ) )
 print( "  System load:\t{}\tProcesses:\t{}".format( load, processes ) )
-print( "  Usage of /:\t{0}\t\tMemory Usage:\t{1:.2f}%".format( strg,  freeMem / ttlMem * 100  ) )
+print( "  Usage of /:\t{0}\t\tMemory Usage:\t{1:.2f}%".format( strg,  ( 1 - freeMem / ttlMem ) * 100  ) )
