01994a7522e80b37a8cf25427ef53c9efe56b7f2
itbaldwi <itbaldwi@ncsu.edu>
2016-04-20 18:12:02 -0400

    Test Sys_info

--- Utils/sys_info.py
+++ Utils/sys_info.py
@@ -37,11 +37,11 @@ wMem = float( runCommand( "memory_pressure | grep 'Pages wired down' | awk '{ pr
 cMem = float( runCommand( "memory_pressure | grep 'Pages used by compressor:' | awk '{ print $5*4096/1024^3 }'" ) )
 tMem = float( runCommand( "sysctl -a | grep memmap.Conventional | awk '{ print $2/1024^3 }'" ) )
 uptime = runCommand( "uptime | awk -F'[:, ]' '{ printf \"%s days, %sh%sm\", $5, $8, $9 }'" );
-network = runCommand( "ping -c 1 google.com | grep -c '1 packets received' | awk '{ gsub( \"1\", \"Up\", $1 ); gsub( \"0\", \"Down\"); print $1 }'" )
+#network = runCommand( "ping -c 1 google.com | grep -c '1 packets received' | awk '{ gsub( \"1\", \"Up\", $1 ); gsub( \"0\", \"Down\"); print $1 }'" )
 
 
 print( "\n  System information as of {}\n".format( date ) )
 print( "  System load:\t{}\tProcesses:\t{}".format( load, processes ) )
 print( "  Usage of /:\t{0}\t\tMemory Usage:\t{1:.2f}%".format( strg,  (aMem + wMem + cMem )/ tMem * 100  ) )
-print( "  Uptime:\t{}\t\tNetwork:\t{}\n".format( uptime, network ) )
+print( "  Uptime:\t{}\t\t\n".format( uptime) )
 print( "  {} packages can be updated.\n".format( updates ) )
