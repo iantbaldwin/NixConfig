bcbfb3bbc3b2f2947c8e53371d0346c43ba8a4c3
itbaldwi <itbaldwi@ncsu.edu>
2016-05-09 05:52:54 -0400

    Fixed uptime

--- Utils/sys_info.py
+++ Utils/sys_info.py
@@ -27,6 +27,7 @@ To Do:
 """
 import subprocess
 import os
+import re
 
 def runCommand( command ):
     process = subprocess.Popen( command, shell=True, stdout=subprocess.PIPE )
@@ -41,12 +42,15 @@ aMem = float( runCommand( "memory_pressure | grep 'Pages active:' | awk '{ print
 wMem = float( runCommand( "memory_pressure | grep 'Pages wired down' | awk '{ print $4*4096/1024^3 }'" ) )
 cMem = float( runCommand( "memory_pressure | grep 'Pages used by compressor:' | awk '{ print $5*4096/1024^3 }'" ) )
 tMem = float( runCommand( "sysctl -a | grep memmap.Conventional | awk '{ print $2/1024^3 }'" ) )
-uptime = runCommand( "uptime | awk -F'[:, ]' '{ printf \"%s days, %sh%sm\", $5, $8, $9 }'" );
+uptime = re.split( r'^[\s]', re.split( r', [\d] user', re.split( r'up[\s\s]', runCommand( "uptime" ) )[ 1 ] )[ 0 ] )[ 1 ]
+if uptime.find( ":" ) != -1:
+    uptime = uptime.replace( ":", "h" )
+    uptime += "m"
 network = runCommand( "ifconfig | grep 'inet 1[097]' | awk 'FNR==1 { print $2 }'" )
 
 
 print( "\n  System information as of {}\n".format( date ) )
 print( "  System load:\t{}\tProcesses:\t{}".format( load, processes ) )
 print( "  Usage of /:\t{0}\t\tMemory Usage:\t{1:.2f}%".format( strg,  (aMem + wMem + cMem )/ tMem * 100  ) )
-print( "  Uptime:\t{}\t\tIP Address:\t{}\n".format( uptime, network) )
+print( "  Uptime:\t{0:8}\t\tIP Address:\t{1}\n".format( uptime, network) )
 print( "  {} packages can be updated.\n".format( updates ) )
