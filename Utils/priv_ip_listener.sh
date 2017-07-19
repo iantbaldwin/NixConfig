519336fcb2d6b482a7bc69db6a0175c1fc0a4140
Ian Baldwin <iantbaldwin@elektrikfish.com>
2017-07-19 12:01:14 -0400

    Changed public ip listener to private

---
+++ Utils/priv_ip_listener.sh
@@ -0,0 +1,14 @@
+while true; do
+	value=$(ipconfig getifaddr en0)
+	if [ ! -z $value ]; then
+		old_value=$(cat ~/.cache/priv_ip)
+		if [[ $value != $old_value ]]; then
+			echo $value > ~/.cache/priv_ip
+			tmux refresh-client -S > /dev/null 2>&1
+		fi
+	else
+		echo "No LAN" > ~/.cache/priv_ip
+		tmux refresh-client -S > /dev/null 2>&1
+	fi
+	sleep 10
+done
